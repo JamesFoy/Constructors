467331207e6123fe29b77b5d330293884f15eac7
James Home <jamesfoy17>
2019-05-06 17:16:25 +0100

    Complete Constructor & Constructor Overloading

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Enemy
+{
+    public Enemy(int health, string name, int score) : this()
+    {
+        this.Health = health;
+        this.Name = name;
+        this.Score = score;
+    }
+
+    public Enemy(int score) : this(3, "Goblin", score)
+    {
+
+    }
+
+    public int Health { get; set; }
+    public string Name { get; set; }
+    public int Score { get; set; }
+
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    Enemy enemy;
+    Enemy enemy2;
+
+    public GameObject enemyObject;
+    public GameObject enemy2Object;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            enemy = new Enemy(100, "Orc", 10);
+            enemyObject.SetActive(true);
+            Debug.Log("An enemy has spawned, it is a " + enemy.Name + " with the health of " + enemy.Health + " and gives a score of " + enemy.Score + " if you kill it!!!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            enemy2 = new Enemy(5);
+            enemy2Object.SetActive(true);
+            Debug.Log("An enemy has spawned, it is a " + enemy2.Name + " with the health of " + enemy2.Health + " and gives a score of " + enemy2.Score + " if you kill it!!!");
+        }
+    }
+}
